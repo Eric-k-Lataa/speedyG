40e1289f05c4330741680099c2b592be74768ad1
Caro <carolina.consuelos@live.com.mx>
2026-09-19 02:23:31 -0600

    fix: correccion maquina de estados y formato de commit 4fe00c0

--- src/state_machine.rs
+++ src/state_machine.rs
@@ -1,47 +1,35 @@
 #[derive(Debug)]
 pub enum JobState {
-	Queued,
-	Running,
-	Succeeded,
-	Failed,
-	Canceled,
+    Queued,
+    Running,
+    Succeeded,
+    Failed,
+    Canceled,
 }
 
 pub enum JobEvent {
-	AcceptedWithCapacity,
-	AcceptedWithoutCapacity,
-	CapacityAvailable,
-	Completed,
-	Error,
-	Cancel,
+    CapacityAvailable,
+    Completed,
+    Error,
+    Cancel,
 }
 
-pub fn initial_state(has_capacity:bool) ->JobState {
-	if has_capacity {
-		JobState::Running
-	} else {
-		JobState::Queued
-	}
-
+pub fn initial_state(has_capacity: bool) -> JobState {
+    if has_capacity {
+        JobState::Running
+    } else {
+        JobState::Queued
+    }
 }
-
 
 impl JobState {
-	pub fn transition(self, event: JobEvent) -> Option<JobState> {
-		match (self,event){
-			(JobState::Queued, JobEvent::CapacityAvailable) =>{
-				Some(JobState::Running)
-			}, 
-			(JobState::Running, JobEvent::Completed) =>{
-                                Some(JobState::Succeeded)
-			},
-			(JobState::Running, JobEvent::Error) =>{
-                                Some(JobState::Failed)
-			},
-			(JobState::Running, JobEvent::Cancel) =>{
-                                Some(JobState::Canceled)			
-			},
-			_ => None,
-		}
-	}
+    pub fn transition(self, event: JobEvent) -> Option<JobState> {
+        match (self, event) {
+            (JobState::Queued, JobEvent::CapacityAvailable) => Some(JobState::Running),
+            (JobState::Running, JobEvent::Completed) => Some(JobState::Succeeded),
+            (JobState::Running, JobEvent::Error) => Some(JobState::Failed),
+            (JobState::Running, JobEvent::Cancel) => Some(JobState::Canceled),
+            _ => None,
+        }
+    }
 }
