b6e5bd015481afd299b975c5bac0932b05019066
Carolina Consuelos <111407046+CaroConsbon@users.noreply.github.com>
2026-09-23 23:19:10 -0600

    Caro (#33)
    * fix: campos privados en Job y warning (FSM)
    
    * Dependencias

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,44 @@
 mod state_machine;
 
 use state_machine::{JobEvent, JobState, new_job};
+use tracing_subscriber;
 
 fn main() {
+    tracing_subscriber::fmt::init();
     // Pruebas maquina de estados
     let mut job = new_job(1, "sleep 10".to_string(), false);
     JobState::transition(&mut job, JobEvent::Completed);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 
     let mut job = new_job(1, "sleep 10".to_string(), false);
     JobState::transition(&mut job, JobEvent::Error);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 
     let mut job = new_job(1, "sleep 10".to_string(), false);
     JobState::transition(&mut job, JobEvent::Cancel);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 
     let mut job = new_job(1, "sleep 10".to_string(), true);
     JobState::transition(&mut job, JobEvent::CapacityAvailable);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 
     //Pruebas de invalidas
 
     let mut job = new_job(1, "sleep 10".to_string(), true);
     JobState::transition(&mut job, JobEvent::Completed);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 
     let mut job = new_job(1, "sleep 10".to_string(), false);
     JobState::transition(&mut job, JobEvent::CapacityAvailable);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 
     let mut job = new_job(1, "sleep 10".to_string(), false);
     JobState::transition(&mut job, JobEvent::Completed);
     JobState::transition(&mut job, JobEvent::Error);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 
     let mut job = new_job(1, "sleep 10".to_string(), false);
     JobState::transition(&mut job, JobEvent::Error);
     JobState::transition(&mut job, JobEvent::Completed);
-    println!("{:?}", job.state);
+    println!("{:?}", job.get_state());
 }
