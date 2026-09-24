913f471d5c9d33b723d8bdadf2f1a301fabfbc07
Caro <carolina.consuelos@live.com.mx>
2026-09-23 23:09:02 -0600

    fix: campos privados en Job y warning (FSM)

--- src/state_machine.rs
+++ src/state_machine.rs
@@ -1,3 +1,5 @@
+use tracing::warn;
+
 #[derive(Debug, Clone, Copy)]
 pub enum JobState {
     Queued,
@@ -7,6 +9,7 @@ pub enum JobState {
     Canceled,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub enum JobEvent {
     CapacityAvailable,
     Completed,
@@ -15,10 +18,9 @@ pub enum JobEvent {
 }
 
 pub struct Job {
-    pub id: u32,
-    pub command: String,
+    id: u32,
+    command: String,
     state: JobState,
-    pub waiting: bool,
 }
 
 pub fn new_job(id: u32, command: String, waiting: bool) -> Job {
@@ -28,26 +30,34 @@ pub fn new_job(id: u32, command: String, waiting: bool) -> Job {
         JobState::Running
     };
 
-    Job {
-        id,
-        command,
-        state,
-        waiting,
+    Job { id, command, state }
+}
+
+impl Job {
+    pub fn get_id(&self) -> u32 {
+        self.id
+    }
+    pub fn get_command(&self) -> &str {
+        &self.command
+    }
+
+    pub fn get_state(&self) -> JobState {
+        self.state
     }
 }
 
 impl JobState {
     pub fn transition(job: &mut Job, event: JobEvent) {
         match (job.state, event) {
-            (JobState::Queued, JobEvent::CapacityAvailable) => {
-                job.state = JobState::Running;
-                job.waiting = false;
-            }
-
+            (JobState::Queued, JobEvent::CapacityAvailable) => job.state = JobState::Running,
+            (JobState::Queued, JobEvent::Cancel) => job.state = JobState::Canceled,
             (JobState::Running, JobEvent::Completed) => job.state = JobState::Succeeded,
             (JobState::Running, JobEvent::Error) => job.state = JobState::Failed,
             (JobState::Running, JobEvent::Cancel) => job.state = JobState::Canceled,
-            _ => {}
+            _ => warn!(
+                "Transicion de estado invalida: {:?} + {:?}",
+                job.state, event
+            ),
         }
     }
 }
